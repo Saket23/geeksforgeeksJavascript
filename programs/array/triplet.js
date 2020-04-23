26e306943292fc8502a0839c08f65e6811fe55e9
Saket Sinha <saketsinha23@gmail.com>
2020-04-24 01:27:14 +0530

    triplet

--- programs/array/triplet.js
+++ programs/array/triplet.js
@@ -3,21 +3,22 @@
 //https://www.geeksforgeeks.org/count-triplets-with-sum-smaller-that-a-given-value/
 
 function findTriplet(array) {
+  let obj = {};
+  let sum = 0;
   let count = 0;
-  for (let i = 0; i < array.length - 2; i++) {
+  for (let i = 0; i < array.length - 1; i++) {
     for (let j = 0; j < array.length; j++) {
-      if (j !== i) {
-        for (let k = j + 1; k < array.length; k++) {
-          if (k !== i) {
-            if (array[i] === array[j] + array[k]) {
-              count++;
-            }
-          }
-        }
-      }
+      sum = array[i] + array[j];
+      obj[sum] = 1;
+    }
+  }
+  for (let k = 0; k < array.length; k++) {
+    if (obj[array[k]]) {
+      count = count + 1;
     }
   }
   if (count === 0) return -1;
+
   return count;
 }
 
